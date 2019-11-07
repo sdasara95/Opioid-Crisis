59a26d35e99640b1efd51f5ca9cd7177dd0cf7d7
Satya Dasara <sdasara95@gmail.com>
2019-11-06 20:52:33 -0500

    Update process.py

--- process.py
+++ process.py
@@ -12,3 +12,39 @@ os.chdir(r'C:\Users\Satya\Desktop\dviz\arcos_all_washpost.tsv')
 df = pd.read_table('arcos_all_washpost.tsv', sep='\t',chunksize=chunksize, header=0)
 
 df1 = next(df)
+
+df1['TRANSACTION_DATE']
+
+def fetch_date(df,row_number,col='TRANSACTION_DATE'):
+    date = df[col].iloc[row_number]
+    date = str(date)
+    year = date[-4:]
+    day = date[-6:-4]
+    
+    year_day_str_len = len(list(year)) + len(list(day))
+    month_len = len(date) - year_day_str_len
+    
+    month = date[0:month_len]
+    
+    year = int(year)
+    day = int(day)
+    month = int(month)
+    
+    return year,month
+
+def fetch_col(df,row_number,col,fetch_date=fetch_date):
+    if col not in df.columns:
+        raise Exception('Invalid Column')
+    value = df[col].iloc[row_number]
+    
+    if col=='TRANSACTION_DATE':
+        year,month = fetch_date(df,row_number,col)
+        return year,month
+    
+    try:
+        value = int(value)
+    except:
+        value = str(value)
+    return value
+
+fetch_col(df1,1,'REPORTER_DEA_NO')
